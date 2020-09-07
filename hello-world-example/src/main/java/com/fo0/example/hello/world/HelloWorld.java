c528bf4c9debb28e7f7da48dae1cf1f632edee2b
fo0 <fo0@fo0.me>
2020-09-07 14:54:08 -0700

    fix

--- hello-world-example/src/main/java/com/fo0/example/hello/world/HelloWorld.java
+++ hello-world-example/src/main/java/com/fo0/example/hello/world/HelloWorld.java
@@ -3,7 +3,7 @@ package com.fo0.example.hello.world;
 public class HelloWorld {
 
 	public static void print() {
-		System.out.println("Hello World 13");
+		System.out.println("Hello World 14");
 	}
 
 }
